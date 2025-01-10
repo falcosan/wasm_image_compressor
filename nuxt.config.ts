d52cfd73a0fb71a5c758fd6b856eab6c8171783c
Daniele Falchetti <danielefalche@gmail.com>
2025-01-11 00:12:37 +0100

    chore: rename project to wasm_image_compressor and update version to 1.0.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,6 @@
 export default defineNuxtConfig({
   modules: ["@vueuse/nuxt", "@nuxt/eslint", "@nuxtjs/tailwindcss"],
-
-  future: {
-    compatibilityVersion: 4,
-  },
-
-  experimental: {
-    payloadExtraction: false,
-  },
-
   devtools: { enabled: false },
   compatibilityDate: "2025-01-10",
+  future: { compatibilityVersion: 4 },
 });
