af734aa32b509190da7c1179cd3807e1e6cdbdf2
Daniele Falchetti <danielefalche@gmail.com>
2025-01-12 18:54:56 +0100

    feat: update publish script to change output directory and include README and package.json

--- publish.sh
+++ publish.sh
@@ -2,11 +2,16 @@
 
 cd wasm
 
-wasm-pack build --out-name index --target web --out-dir dist
+wasm-pack build --out-name index --target web --out-dir pkg/lib
 
-cd dist
+cd pkg/lib
 
-npm publish --access=public
+cp ../../../README.md ../
+
+mv .gitignore ../
+mv package.json ../
 
 cd ..
+jq '.files = ["lib/"] | .main = "lib/index.js" | .types = "lib/index.d.ts"' package.json > temp_package.json && mv temp_package.json package.json
 
+npm publish --access=public
