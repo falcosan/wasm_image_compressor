8f230de688408faa0c08f3df37b456fb1e7d671c
Daniele Falchetti <danielefalche@gmail.com>
2025-01-13 12:37:30 +0100

    feat: refactor image processing functions for improved clarity and performance; update types for consistency

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,6 @@
 use error::ConvertError;
-use image::ImageFormat;
-use js_sys::Uint8Array;
+use image::{DynamicImage, ImageFormat};
+use js_sys::{Function, Uint8Array};
 use media_type::MediaType;
 use pixlzr::{FilterType, Pixlzr};
 use std::io::Cursor;
@@ -15,35 +15,27 @@ async fn fetch_image(url: &str) -> Result<Vec<u8>, JsValue> {
     let opts = RequestInit::new();
     opts.set_method("GET");
     opts.set_mode(RequestMode::Cors);
-
     let request = Request::new_with_str_and_init(url, &opts)?;
-
     let window = web_sys::window().ok_or("No window available")?;
-    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
-    let resp: Response = resp_value.dyn_into()?;
-
+    let resp: Response = JsFuture::from(window.fetch_with_request(&request))
+        .await?
+        .dyn_into()?;
     let data = JsFuture::from(resp.array_buffer()?).await?;
-    let uint8_array = Uint8Array::new(&data);
-
-    Ok(uint8_array.to_vec())
+    Ok(Uint8Array::new(&data).to_vec())
 }
 
-fn load_image(
-    file: &[u8],
-    source_type: Option<MediaType>,
-) -> Result<image::DynamicImage, ConvertError> {
+fn load_image(file: &[u8], source_type: Option<MediaType>) -> Result<DynamicImage, ConvertError> {
     match source_type {
         Some(MediaType::Raster(file_type)) => {
-            Ok(image::load_from_memory_with_format(file, file_type)?)
+            image::load_from_memory_with_format(file, file_type).map_err(ConvertError::from)
         }
-        None => Ok(image::load_from_memory(file).map_err(|_| {
-            ConvertError::UnknownFileType("Failed to load image from memory".into())
-        })?),
+        None => image::load_from_memory(file)
+            .map_err(|_| ConvertError::UnknownFileType("Failed to load image from memory".into())),
     }
 }
 
 fn write_image(
-    img: &image::DynamicImage,
+    img: &DynamicImage,
     file_type: Option<ImageFormat>,
     compression_factor: f32,
 ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
@@ -57,23 +49,24 @@ fn write_image(
 }
 
 fn process_image(
-    img: image::DynamicImage,
+    img: DynamicImage,
     source_type: Option<ImageFormat>,
     target_type: Option<ImageFormat>,
-) -> image::DynamicImage {
+) -> DynamicImage {
     let t = target_type.unwrap_or(ImageFormat::WebP);
-    let i = match source_type {
-        Some(ImageFormat::Hdr) => image::DynamicImage::ImageRgba8(img.to_rgba8()),
-        _ => img,
+    let i = if source_type == Some(ImageFormat::Hdr) {
+        DynamicImage::ImageRgba8(img.to_rgba8())
+    } else {
+        img
     };
     match t {
         ImageFormat::Jpeg
         | ImageFormat::Qoi
         | ImageFormat::Farbfeld
         | ImageFormat::Pnm
-        | ImageFormat::Tga => image::DynamicImage::ImageRgb8(i.to_rgb8()),
+        | ImageFormat::Tga => DynamicImage::ImageRgb8(i.to_rgb8()),
         ImageFormat::Ico => i.resize(256, 256, image::imageops::FilterType::Lanczos3),
-        ImageFormat::OpenExr => image::DynamicImage::ImageRgba32F(i.to_rgba32f()),
+        ImageFormat::OpenExr => DynamicImage::ImageRgba32F(i.to_rgba32f()),
         _ => i,
     }
 }
@@ -84,7 +77,7 @@ pub async fn convert_image(
     src_type: &str,
     target_type: &str,
     compression: f32,
-    cb: &js_sys::Function,
+    cb: &Function,
 ) -> Result<Uint8Array, JsValue> {
     let this = JsValue::NULL;
     let _ = cb.call2(
@@ -93,13 +86,12 @@ pub async fn convert_image(
         &JsValue::from_str("Starting conversion"),
     );
     let file_data = if file_input.is_string() {
-        let url = file_input.as_string().unwrap();
         let _ = cb.call2(
             &this,
             &JsValue::from_f64(35.0),
             &JsValue::from_str("Fetching image"),
         );
-        fetch_image(&url).await?
+        fetch_image(&file_input.as_string().unwrap()).await?
     } else if file_input.is_instance_of::<Uint8Array>() {
         Uint8Array::new(file_input).to_vec()
     } else {
